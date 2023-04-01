06f04f076a870689bf3ec873c56a523f3eb8daec
Vodziz <126247353+Vodziz@users.noreply.github.com>
2023-04-01 23:06:42 +0000

    Create sdiusahaui.lua

--- sdiusahaui.lua
+++ sdiusahaui.lua
@@ -1,21 +1,68 @@
-_G.test = true
-while _G.test do
-    wait()
-if game:GetService("Players").LocalPlayer.PlayerGui.Interface.Score.Frame.Jobs.Visible == false then
-wait(0.5)
-game:GetService("ReplicatedStorage").Systems.Jobs.StartJob:InvokeServer("TrailerDelivery", "Dealership")
-wait(0.5)
-elseif game:GetService("Players").LocalPlayer.PlayerGui.Interface.Score.Frame.Jobs.Visible == true then
-for i,v in pairs(game:GetService("Workspace").Cars:GetDescendants()) do
-    if v.Name == "Owner" and v.Value == game.Players.LocalPlayer and game.Players.LocalPlayer:DistanceFromCharacter(game:GetService("Workspace").CompletionRegion.Primary.Position) > 25 then
-v.Parent:SetPrimaryPartCFrame(game:GetService("Workspace").CompletionRegion.Primary.CFrame*CFrame.new(0,5,-20))
-wait()
+local _, library = pcall(loadstring(game:HttpGet("https://raw.githubusercontent.com/TrixAde/Osmium/main/OsmiumLibrary.lua")))
+
+local window = library:CreateWindow("Drive World Farm DEEX#8535")
+local tab = window:CreateTab("Farm")
+local tabCredits = window:CreateTab("Credits")
+
+local togglefarm = false
+local pausedelay = 60
+local farmvalue = false
+
+task.spawn(function()
+	repeat task.wait() until not mouse.Target:FindFirstChild("AttachmentHighlight")
+	-- Code
+end)
+
+function delayactivate()
+    pcall(function()
+            task.spawn(function()
+        task.wait(pausedelay)
+        togglefarm = false 
+        task.wait(4)
+        if farmvalue == true then 
+            togglefarm = true
+        end
+        if togglefarm == true then
+            delayactivate()
+        end
+    end)
+    end)
 end
+
+local toggle = tab:CreateToggle("Farm", false, function (value)
+    if value == true then 
+        delayactivate() 
+    end
+    togglefarm = value
+    farmvalue = value
+end)
+
+local textbox = tab:CreateTextbox("Collect Money Delay (In seconds)", function(value)
+    pausedelay = tonumber(value)
+end, "Write number here")
+
+local button = tabCredits:CreateButton("Osmium Library", function()
+    pcall(function()
+        setclipboard("https://github.com/TrixAde/Osmium/blob/main/OsmiumLibraryDocumentation.md")
+    end)
+end)
+
+
+while true do 
+    pcall(function()
+            if togglefarm == true then 
+        game:GetService("Workspace").Gravity = 500
+
+        for i, v in pairs(game:GetService("Workspace").Cars:GetChildren()) do
+            if tostring(v.Owner.Value) == game:GetService("Players").LocalPlayer.Name then 
+                v.Main.CFrame = CFrame.new(0, 500, 0)
+                wait(1)
+            end
+        end
+    else 
+        game:GetService("Workspace").Gravity = 196.2
+        wait()
+    end
+    end)
 end
-if game:GetService("Players").LocalPlayer.PlayerGui.Interface.JobComplete.Enabled == true then
-game:GetService("ReplicatedStorage").Systems.Jobs.CashBankedEarnings:FireServer()
-game:GetService("Players").LocalPlayer.PlayerGui.Interface.JobComplete.Enabled = false
-game:GetService("Lighting").BackgroundBlur.Enabled = false
-end
-end
-end
+
